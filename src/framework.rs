2aee92be4acc0b259c6580c9e6fb9759525e9fb4
Komorio <twilivez@gmail.com>
2022-06-28 00:50:47 +0900

    🔨 Add features and a test feature
    - Add render and release_render functions
    
    - Change self parameters to muetable
    
    - Make update() be updated test index value

--- src/framework.rs
+++ src/framework.rs
@@ -3,6 +3,7 @@ use std::{thread::sleep, time::Duration};
 pub struct App {
     progress: bool,
     frame: Duration,
+    index: u8,
 }
 
 impl App {
@@ -10,18 +11,30 @@ impl App {
         App {
             progress: true,
             frame: Duration::from_secs_f32(1.0 / 30.0),
+            index: 0,
         }
     }
 
-    pub fn execute(&self) {
+    pub fn execute(&mut self) {
         while self.progress {
             self.update();
+            self.render();
             sleep(self.frame);
+            self.release_render();
         }
     }
 
-    fn update(&self) {
-        // TODO : Make update
-        println!("UPDATED");
+    fn update(&mut self) {
+        self.index += 1;
+    }
+
+    fn render(&self) {
+        println!("{}", self.index);
+    }
+
+    fn release_render(&self) {
+        std::process::Command::new("clear")
+            .status()
+            .expect("Fail to clear screen.");
     }
 }
